ff6cc624f7553b31d78b3d7e2ca719275349aee7
scottkoons <scottkoons@gmail.com>
2021-05-28 12:52:43 -0600

    addCommas

--- addCommas.js
+++ addCommas.js
@@ -1,3 +1,21 @@
-function addCommas() {}
+function addCommas(num) {
+  // make a reverse array of the absolute value of the 'num'
+  let split = ("" + Math.abs(num)).split("").reverse();
+
+  // check if 'num' is negative
+  // add negative sign into string if 'num' is negative
+  let str = num < 0 ? "-" : "";
+
+  // start building string of 'num'
+  for (let i = split.length - 1; i >= 0; i--) {
+    str += split[i];
+    // add comma into string in correct location
+    if (i % 3 === 0 && i !== 0) {
+      str += ",";
+    }
+  }
+
+  return str;
+}
 
 module.exports = addCommas;
--- addCommas.test.js
+++ addCommas.test.js
@@ -1,7 +1,28 @@
 const addCommas = require("./addCommas");
 
 describe("#addCommas", () => {
+  /** 'addCommas' should be a function */
   test("it is a function", () => {
     expect(typeof addCommas).toBe("function");
+  });
+
+  test("it should return a string", () => {
+    expect(typeof addCommas(1)).toEqual("string");
+  });
+
+  test("it should return a positive number as a string with commas", () => {
+    expect(addCommas(1234)).toBe("1,234");
+    expect(addCommas(1000000)).toBe("1,000,000");
+    expect(addCommas(9876543210)).toBe("9,876,543,210");
+  });
+
+  test("it should return a string with no commas", () => {
+    expect(addCommas(6)).toBe("6");
+    expect(addCommas(678)).toBe("678");
+  });
+
+  test("it should return a negative number as a string with '-' and commas", () => {
+    expect(addCommas(-10)).toBe("-10");
+    expect(addCommas(-5678)).toBe("-5,678");
   });
 });
